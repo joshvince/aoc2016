c4da91c2bef6b9d54e753f0b5d749e44431010c0
Josh <joshmvince@gmail.com>
2016-12-02 13:46:38 +0000

    amended the file naming structure and creates a solver function to be used in node

---
+++ README.md
@@ -0,0 +1,7 @@
+## Advent of Code 2016
+
+### To get the answers to a puzzle:
+From the root of this directory, run the following commands (replace day number with the number.)
+`node`
+`var solution = require('./solve.js')`
+`solution.run(${day number})`
--- solve.js
+++ solve.js
@@ -1,8 +1,18 @@
 var fs = require('fs');
 require('babel-register');
 
-function loadFile(day, filename){
-  var filepath = `${__dirname}/day${day}/${filename}`
+function solve(day){
+  var directoryPath = `${__dirname}/day${day}`
+  loadInput(day, directoryPath).then(data => {
+    var Solution = require(`${directoryPath}/solution.js`)
+    console.log(Solution.solve(data))
+  }).catch(error => {
+    console.log(error)
+  })
+}
+
+function loadInput(day, directory){
+  var filepath = `${directory}/input.txt`
   return new Promise((resolve, reject) => {
     fs.readFile(filepath, function(err, data){
       if (err) {
@@ -15,8 +25,22 @@ function loadFile(day, filename){
   })
 }
 
-loadFile(1, 'day1solution.js').then(data => {
-  console.log(data)
-}).catch(error => {
-  console.log(error)
-})
+function run(obj, callback){
+  obj.y += 5
+  if (typeof callback === 'function') {
+    var res = callback(obj);
+    console.log("returned the following: " + JSON.stringify(res))
+    return res;
+  }
+}
+
+function step1(obj){
+  obj.x += 5
+  return obj
+}
+
+
+module.exports = {
+  run: solve
+}
+
