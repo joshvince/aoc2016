00e40c623911313e6ee52e18d997eb4ac7de058e
Josh <joshmvince@gmail.com>
2016-12-05 08:48:51 +0000

    almost solved - one more func to do

--- day2/solution.js
+++ day2/solution.js
@@ -4,9 +4,28 @@ function parseInput(longString){
   })
 }
 
+function getAllNumbers(init, directions, callback) {
+  var code = [init];
+  directions.forEach((array, i) => {
+    var num = getOneNumber(code[i], array, callback)
+    code.push(num);
+  })
+  return code
+}
+
+function getOneNumber(init, directionsArray, callback) { 
+  return directionsArray.reduce((acc, dir) => { 
+    return callback(acc, dir);
+  }, init)
+}
+
+// PART ONE:
+
 function solvePartOne(input){
   var directions = parseInput(input)
-  return getAllNumbers(5, directions)
+  var codeArray = getAllNumbers(5, directions, move)
+  codeArray.shift()
+  return parseInt(codeArray.join(""))
 }
 
 /*
@@ -15,26 +34,6 @@ moving down adds 3 unless the start button is > 6
 moving right adds 1 unless the start button is a multiple of 3
 moving left minuses 1 unless the answer is < 1 or a multiple of 3
 */
-
-function getAllNumbers(init, directions) {
-  var code = [init];
-  directions.forEach((array, i) => {
-    var num = getOneNumber(code[i], array)
-    code.push(num);
-  })
-  return parseCode(code)
-}
-
-function parseCode(array) {
-  array.shift();
-  return parseInt(array.join(""))
-}
-
-function getOneNumber(init, directionsArray) { 
-  return directionsArray.reduce((acc, dir) => { 
-    return move(acc, dir);
-  }, init)
-}
 
 function move(pos, direction) {
   switch (direction) {
@@ -91,10 +90,86 @@ function catchRight(prev, next) {
   return verdict
 }
 
+/*
+PART TWO
+*/
+
+/*
+
+[[x,x,1,x,x],[x,2,3,4,x],[5,6,7,8,9],[x,a,b,c,x],[x,x,d,x,x]]
+
+'D' moves you one more array along the pad array
+'R' moves you one index along the existing array
+'L' moves you one index previous along the existing array
+'U' moves you one array previous along the pad array
+if you ever get an 'undefined', you went wrong, and should stay where you are.
+*/
+
+function solvePartTwo(input) {
+  var directions = parseInput(input)
+  var codeArray = getAllNumbers([2,0], directions, executeOneCommand)
+  codeArray.shift()
+  return printCode(codeArray)
+}
+
+function printCode(positions){
+  var keypad = generateKeypad()
+  var code = [];
+  positions.forEach(arr => {
+    var row = arr[0],
+    key = arr[1]
+    code.push(keypad[row][key])
+  })
+  return code.join("")
+}
+
+function generateKeypad(){
+  return [[,,1,,],[,2,3,4,],[5,6,7,8,9],[,'a','b','c',],[,,'d',,]]
+}
+
+function executeOneCommand(posArray, direction){
+  var row = posArray[0],
+  pos = posArray[1],
+  newPos;
+  switch (direction) {
+    case 'U':
+      newPos = validMove(row-1, pos)
+      break;
+    case 'R':
+      newPos = validMove(row, pos+1)
+      break;
+    case 'D':
+      newPos = validMove(row+1, pos)
+      break;
+    case 'L':
+      newPos = validMove(row, pos-1)
+      break;
+    default:
+      throw new Error ("the input was not a valid direction")
+      break;
+  }
+  return newPos ? newPos : posArray
+}
+
+function validMove(row, pos){
+  var keypad = generateKeypad()
+  if (typeof keypad[row][pos] === 'undefined'){
+    return false
+  }
+  else {
+    return [row, pos]
+  }
+}
+
 module.exports = {
   parseInput: parseInput,
   failsafe: failsafe,
+  move: move,
   getOneNumber: getOneNumber,
   getAllNumbers: getAllNumbers,
-  solvePartOne: solvePartOne
+  solvePartOne: solvePartOne,
+  generateKeypad: generateKeypad,
+  executeOneCommand: executeOneCommand,
+  printCode: printCode,
+  solvePartTwo: solvePartTwo
 }
--- test/day2.test.js
+++ test/day2.test.js
@@ -5,12 +5,14 @@ var chai = require('chai')
 
 var s = require('../day2/solution.js')
 
+var inputString = 'ULL\nRRDDD\nLURDL\nUUUUD'
+var testInput = ['U', 'L', 'L'];
+var testInput2 = ['R', 'R', 'D', 'D', 'D']
+var testInput3 = ['L', 'U', 'R', 'D', 'L']
+var testInput4 = ['U', 'U', 'U', 'U', 'D']
+
 describe('Part One', function () {
-  var inputString = 'ULL\nRRDDD\nLURDL\nUUUUD'
-  var testInput = ['U', 'L', 'L'];
-  var testInput2 = ['R', 'R', 'D', 'D', 'D']
-  var testInput3 = ['L', 'U', 'R', 'D', 'L']
-  var testInput4 = ['U', 'U', 'U', 'U', 'D']
+  
   describe('#parseInput()', function () {
     var result = s.parseInput(inputString);
     it('correctly parses the input', function () {
@@ -47,14 +49,38 @@ describe('Part One', function () {
   });
   describe('#getOneNumber()', function () {
     it('returns the correct number from the examples', function () {
-      expect(s.getOneNumber(5, testInput)).to.equal(1)
-      expect(s.getOneNumber(1, testInput2)).to.equal(9)
+      expect(s.getOneNumber(5, testInput, s.move)).to.equal(1)
+      expect(s.getOneNumber(1, testInput2, s.move)).to.equal(9)
     })
   });
   describe('#getAllNumbers()', function () {
-    it('returns the correct number from the examples', function () {
+    it('returns the correct numbers from the examples, as an array to be parsed', function () {
       var input = [testInput, testInput2, testInput3, testInput4]
-      expect(s.getAllNumbers(5, input)).to.equal(1985)
+      expect(s.getAllNumbers(5, input, s.move)).to.be.an('array').with.lengthOf(5)
+    })
+  })
+});
+
+describe('Part Two', function () {
+  var keypad = s.generateKeypad();
+  describe('#receiveInstruction()', function () {
+    it('returns an array containing two numbers that are a valid key', function () {
+      expect(s.executeOneCommand([2,0], 'D')).to.be.an('array').with.lengthOf(2)
+      expect(s.executeOneCommand([2,0], 'D')).to.have.members([2,0])
+      expect(s.executeOneCommand([3,3], 'U')).to.have.members([2,3])
+    })
+  })
+  describe('#printCode()', function () {
+    it('converts an array of coordinate positions into a string containing the code', function () {
+      var result = s.printCode([[0,2], [1,1], [1,2]])
+      expect(result).to.be.a('string')
+      expect(result).to.equal('123')
+    })
+  });
+  describe('#solvePartTwo()', function () {
+    it('correctly solves the example code', function () {
+      var result = s.solvePartTwo(inputString)
+      console.log(result)
     })
   })
 })
