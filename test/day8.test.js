3e2855831c6b3c3294db247f6b00c2245aec3a95
Josh <joshmvince@gmail.com>
2016-12-19 18:27:52 +0000

    solves both parts of day 8

--- test/day8.test.js
+++ test/day8.test.js
@@ -6,10 +6,13 @@ var chai = require('chai')
 var s = require('../day8/solution.js')
 
 describe('Day Four', function() {
+  var testInput = "rect 3x2\nrotate column x=1 by 1\nrotate row y=0 by 4\nrotate column x=1 by 1";
   describe('#solvePartOne', function() {
-    var testInput = "rect 3x2\nrotate column x=1 by 1\nrotate row y=0 by 4\nrotate column x=1 by 1"
-    var res = s.solvePartOne(testInput)
-    console.log(JSON.stringify(res));
-    // expect(res).to.equal(6);
+    it('should solve the example input', function() {
+      var res = s.solvePartOne(testInput)
+      // var log = res.map(arr => { return `\n${arr}`})
+      // console.log(`${log}`);
+      expect(res).to.equal(6)
+    });
   });
 });
